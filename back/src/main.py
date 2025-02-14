a5425f04fcbeb3ebfab2133ca30e3768435ee203
fpahsdnjxj <enjin0222@naver.com>
2025-02-15 01:08:22 +0900

    docker compose+connection

--- back/src/main.py
+++ back/src/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import get_currency_info, get_bank_info, get_card_info
+from api.get_currency_info import router as currency_router
+from api.get_bank_info import router as bank_router
+from api.get_card_info import router as card_router
 
-app=FastAPI()
-app.include_router(get_currency_info.router)
-app.include_router(get_bank_info.router)
-app.include_router(get_card_info.router)
+app = FastAPI()
+
+app.include_router(currency_router)
+app.include_router(bank_router)
+app.include_router(card_router)
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:80"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
