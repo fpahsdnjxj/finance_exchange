70b8a0ea2bdde26fc78a5f003d2f3b450600227a
fpahsdnjxj <enjin0222@naver.com>
2025-05-19 23:36:44 +0900

    쓸대 없는 것들 삭제

--- back/src/update_add.py
+++ back/src/update_add.py
@@ -1,22 +1,8 @@
 from update_info.update_currency_info import get_currency_data
-from update_info.add_info import add_info_from_json
-from update_info.update_info import update_info_from_json
-from update_info.temp import add_by_csv
 
-"""
-if(num1==1):
-    get_currency_data()
-elif(num1==2):
-    if(num2==2):
-        add_info()
-"""
 
-#add_info_from_json()
-
-#update_info_from_json()
 
 get_currency_data()
 
-#add_by_csv()
 
 
