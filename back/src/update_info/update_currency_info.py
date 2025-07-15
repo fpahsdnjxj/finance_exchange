ef9a56c0c03c0f532bb6be1dfdf136be9d025377
fpahsdnjxj <enjin0222@naver.com>
2025-07-15 17:12:46 +0900

    update currency api change

--- back/src/update_info/update_currency_info.py
+++ back/src/update_info/update_currency_info.py
@@ -15,40 +15,32 @@ def get_currency_data():
     db_session = next(get_db()) 
     currency_repo = CurrencyRepository(session=db_session)
     now = datetime.now(ZoneInfo("Asia/Seoul"))
-    if now.hour < 11:
-        target_date = (now - timedelta(days=1)).strftime("%Y%m%d")
-    else:
-        target_date = now.strftime("%Y%m%d")
-    api_key=os.getenv("BANK_API_KEY")
-    url=f'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
+    api_key=os.getenv("OPEN_EXCHANGE_API_KEY")
+    url=f'https://openexchangerates.org/api/latest.json'
     params = {
-        "authkey": api_key,  
-        "searchdate": target_date,  
-        "data": "AP01",  
+        "app_id": api_key
     }
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     }
-    response = requests.get(url, params=params, headers=headers, timeout=30, verify="/etc/ssl/certs/ca-certificates.crt")
+    response = requests.get(url, params=params, headers=headers, timeout=30, verify=certifi.where())
 
     if response.status_code==200:
         data = response.json()  
         if not data:
-            print(f"빈 응답 수신: {target_date}, API는 200 OK 응답했으나 데이터 없음")
+            print(f"빈 응답 수신: {now}, API는 200 OK 응답했으나 데이터 없음")
             return
-        for item in data:
-            if "(" in item["cur_unit"]:
-                base_rate = int(item["cur_unit"].split("(")[1].replace(")", ""))
-            else:
-                base_rate = 1
-            currency_code = item["cur_unit"].split("(")[0]
-            country_name = item["cur_nm"]
-            deal_bas_r = float(item["deal_bas_r"].replace(",", ""))/base_rate  # 매매 기준율 (문자열 -> 실수 변환)
+        rates=data.get("rates", {})
+        standard=rates["KRW"]
+        for  currency_code, exchange_rate  in rates.items():
+            currency_info=currency_repo.get_currency_by_currencycode(currency_code=currency_code)
+            if currency_info:
+                country_name = currency_info.country_name
+                deal_bas_r = standard / exchange_rate
+                currency = Currency.create(currency_code=currency_code, country_name=country_name, P_per_Won=deal_bas_r)
+                currency_repo.update_currency_info(currency=currency)
+                print(f"통화 정보 업데이트: {currency_code}, 환율: {deal_bas_r}, 시간: {now}")
 
-            # Currency 객체 생성
-            currency = Currency.create(currency_code=currency_code, country_name=country_name, P_per_Won=deal_bas_r)
-            currency_repo.update_currency_info(currency)
-        print(f"환율 정보 업데이트 완료: {target_date}")
     else:
         print(f"API 요청 실패: {response.status_code}, {response.text}")
 
